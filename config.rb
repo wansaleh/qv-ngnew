e0d3c7befe58f876e7382a0445689923d68c6a95
wansaleh <wansaleh@gmail.com>
2012-11-12 04:53:53 +0800

    Recommit files.

---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'ceaser-easing'
+
+project_path    = File.dirname(__FILE__)
+http_path       = '/'
+sass_dir        = 'app/styles'
+css_dir         = 'public/assets/css'
+images_dir      = 'public/assets/img'
+javascripts_dir = 'public/assets/js'
+output_style    = :nested # :compact, :expanded, :nested, :compressed
